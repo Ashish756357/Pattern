ff5e1a35bb3e07883fb5103cd142f0edc0cca2a0
Ashish Raj <126260804+Ashish756357@users.noreply.github.com>
2023-06-21 12:42:59 +0530

    Create printing pattern.cpp

---
+++ printing pattern.cpp
@@ -0,0 +1,38 @@
+/*
+    1
+   121
+  12321
+ 1234321
+123454321
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int n;
+    cin>>n;
+    
+    int row = 1;5
+    while(row <= n){
+        int space = n - row;
+        while(space){
+            cout<<" ";
+            space--;
+        }
+        int col = 1;
+        while(col <= row){
+            cout<<col;
+            col++;
+        }
+        int start = row - 1;
+        while(start){
+            cout<<start;
+            start--;
+        }
+        cout<<endl;
+        row++;
+    }
+    return 0;
+}
